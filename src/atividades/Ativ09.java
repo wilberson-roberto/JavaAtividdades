56362bba96c13d8c3eb6faefdd96ab3a965176b3
Wilberson Roberto <90524071+wilberson-roberto@users.noreply.github.com>
2022-11-28 02:17:14 -0300

    atividade 09 ok

--- src/atividades/Ativ09.java
+++ src/atividades/Ativ09.java
@@ -1,5 +1,7 @@
 package atividades;
 
+import java.util.Scanner;
+
 public class Ativ09 {
 
 	public static void main(String[] args) {
@@ -9,6 +11,32 @@ public class Ativ09 {
 		 * Ler 10 valores e mostrar quantos desses valores lidos estão no intervalo [10, 20]
 		 * (incluindo os valores 10 e 20 no intervalo) e quantos deles estão fora deste intervalo
 		 */
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Digite um valor qualquer");
+		
+		int i, valor, dentro, fora;
+		
+		valor = 0;
+		dentro = 0;
+		fora = 0;
+		
+		for (i = 10; i <= 20; i++) {
+			
+			valor = entrada.nextInt();
+			
+			if (valor >= 10 && valor <= 20) {
+				dentro++;
+			} else {
+				fora++;
+			}
+		}
+		
+		System.out.println("Dentro do intervalo 10 a 20: " + dentro);
+		System.out.println("Fora do intervalo 10 a 20: " + fora);
+		
+		entrada.close();
 
 	}
 
