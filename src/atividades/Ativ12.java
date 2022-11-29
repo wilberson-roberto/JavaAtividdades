04e33c3cab20e7879a641f5f266f1bcc6fabf0d5
Wilberson Roberto <90524071+wilberson-roberto@users.noreply.github.com>
2022-11-29 15:59:39 -0300

    Atividadde 12 ok

--- src/atividades/Ativ12.java
+++ src/atividades/Ativ12.java
@@ -1,5 +1,7 @@
 package atividades;
 
+import java.util.Scanner;
+
 public class Ativ12 {
 
 	public static void main(String[] args) {
@@ -9,6 +11,25 @@ public class Ativ12 {
 		 * vai parar quando for digitada a idade 999 No final, mostre quantos alunos
 		 * existem na turma e qual é a maior idade do grupo.
 		 */
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		int idade = 0, contador = 0, maiorIdade = 0;
+		
+		do {
+			System.out.println("Digite a idade dos alunos");
+			idade = entrada.nextInt();
+			contador++;
+			
+			if (idade > maiorIdade && idade != 0) {
+				maiorIdade = idade;
+			}
+			
+		} while (idade != 0);
+		
+		System.out.printf("Exitem %d alunos na sala e o aluno mais velho tem %d", contador, maiorIdade);
+		
+		entrada.close();
 
 	}
 
